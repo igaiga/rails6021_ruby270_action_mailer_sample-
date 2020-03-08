d3761cd743f816bab8ab68b2defb9f07aad54e62
Kuniaki Igarashi <igaiga@gmail.com>
2020-03-08 12:11:01 +0900

    Add mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  #config.action_mailer.delivery_method = :file
+  config.action_mailer.delivery_method = :smtp
+  # 保存場所: 指定しないとデフォルトでは tmp/mails 以下に保存される
+  # config.action_mailer.file_settings = { location: Rails.root.join('log/mails') }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
