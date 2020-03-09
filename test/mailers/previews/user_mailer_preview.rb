c44d2a58c3ed632672721590c97970ebf8123e5e
Kuniaki Igarashi <igaiga@gmail.com>
2020-03-09 09:22:37 +0900

    add preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def welcome
+    UserMailer.welcome(User.first)
+#    UserMailer.with(user: User.first).welcome
+  end
 end
