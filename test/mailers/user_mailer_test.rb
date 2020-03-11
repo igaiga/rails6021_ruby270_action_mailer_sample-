6aed1051c3eba2b66062afd16a8c75a1e15c1455
Kuniaki Igarashi <igaiga@gmail.com>
2020-03-11 10:45:02 +0900

    Writing mailer unit test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
-
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome" do
+    user = User.new(name: "igaiga", email: "igaiga@example.com")
+    email = UserMailer.with(user: user).welcome
+    # メールが送信キューに追加されるかどうかをテスト
+    assert_emails(1){ email.deliver_now }
+    # 送信されたメールを確認するテスト
+    assert_equal ["perfect_rails@example.com"], email.from
+    assert_equal ["igaiga@example.com"], email.to
+    assert_equal "登録完了", email.subject
+    assert_includes email.text_part.decoded, user.name
+    assert_includes email.html_part.decoded, user.name
+  end
 end
