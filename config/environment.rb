e2cdcd301505105b6453e3e5e7a24efee946040d
Kuniaki Igarashi <igaiga@gmail.com>
2020-06-13 11:15:59 +0900

    Refactering stmp config

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,14 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  user_name: Rails.application.credentials.smtp_user_name,
-  password: Rails.application.credentials.smtp_password,
-  domain: Rails.application.credentials.smtp_from_domain,
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
+Rails.application.configure do
+  ActionMailer::Base.smtp_settings = {
+    user_name: Rails.application.credentials.smtp_user_name,
+    password: Rails.application.credentials.smtp_password,
+    domain: Rails.application.credentials.smtp_from_domain,
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+end
